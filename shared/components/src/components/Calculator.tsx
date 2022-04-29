7989efaa4f6388144bec351588f6dde116fa2a1b
Steve Beaugé <184604+stevebeauge@users.noreply.github.com>
2022-04-29 09:49:12 +0200

    Working with turborepo OK

--- shared/components/src/components/Calculator.tsx
+++ shared/components/src/components/Calculator.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Double } from '@monorepo/shared-utils';
+import { Double, Half } from '@monorepo/shared-utils';
 import { Label, Stack, TextField } from 'office-ui-fabric-react';
 
 const Calculator = () => {
     const [doubleValue, setDoubleValue] = React.useState(0);
+    const [halfValue, setHalfValue] = React.useState(0);
 
-    const onChange = (newValue: string) => {
+    const onChangeDouble = (newValue: string) => {
         const value = parseFloat(newValue);
         if (!isNaN(value)) {
             setDoubleValue(Double(value));
@@ -14,11 +15,25 @@ const Calculator = () => {
         }
     }
 
+    const onChangeHalf = (newValue: string) => {
+        const value = parseFloat(newValue);
+        if (!isNaN(value)) {
+            setHalfValue(Half(value));
+        } else {
+            setHalfValue(NaN);
+        }
+    };
+    
     return (
         <Stack>
-            <TextField onChange={(evt, newVal) => onChange(newVal || '')} placeholder="Enter a number!" />
-            <Label>{doubleValue}</Label>
-        </Stack>
-    );
+            <Stack>
+                <TextField onChange={(evt, newVal) => onChangeDouble(newVal || '')} placeholder="Enter a number!" />
+                <Label>{doubleValue}</Label>
+            </Stack>
+            <Stack>
+                <TextField onChange={(evt, newVal) => onChangeHalf(newVal || '')} placeholder="Enter a number!" />
+                <Label>{halfValue}</Label>
+            </Stack>
+        </Stack>);
 }
 export { Calculator };
--- shared/utils/src/Maths.ts
+++ shared/utils/src/Maths.ts
@@ -1,1 +1,3 @@
 export const Double = (a: number) => 2 * a;
+
+export const Half = (a: number) => a / 2;
